32c78349091462ddc901c435a8e20b8b334dc254
Alexander Dolgan <5704737@gmail.com>
2022-07-03 16:23:52 +0300

    bankets

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,18 @@
  * Third party
  */
 
-//  = ../../bower_components/jquery/dist/jquery.min.js
-//  = ../../bower_components/slick-master/slick/slick.min.js
-//  = ../../bower_components/jquery.scrollTo-2.1.2/jquery.scrollTo.min.js
-
-//= ../../bower_components/lightgallery/src/js/lightgallery.js
-//= ../../bower_components/lightgallery/lib/jquery.mousewheel.min.js
 
 
+//= ../../bower_components/jquery/dist/jquery.min.js
+// = ../../bower_components/slick-carousel/slick/slick.min.js
+// = ../../bower_components/jquery.scrollTo-2.1.2/jquery.scrollTo.min.js
+
+// = ../../bower_components/lightgallery/src/js/lightgallery.js
+// = ../../bower_components/lightgallery/lib/jquery.mousewheel.min.js
 
 /*
  * Customm
  */
 
+//= ./partials/app.js
 
-
-//= partials/app.js
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,7 +1,23 @@
-window.onload = function() {
+// window.onload = function() {
 
-};
+// };
 
+$( document ).ready(function() {
+  $('.loader').css({
+		opacity : 0,
+		visibility: 'hidden'
+	});
+	
+	$('.b-slider-about').slick( {
+		dots: true,
+		autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    lazyLoad: 'ondemand'
+	});
+  
+  console.log('hi, slick');
+});
 
 
 
